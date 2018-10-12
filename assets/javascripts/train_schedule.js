55619e6ddc30fd7e6ea63c704528bf8f1f79ece9
Duncan Dam <duncandam@duncans-mbp.lan>
2018-10-11 22:33:32 -0500

    JS HTML update

--- assets/javascripts/train_schedule.js
+++ assets/javascripts/train_schedule.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
     //initialize firebase connection
     var config = {
         apiKey: "AIzaSyC_C2yBW6xyfYsyGY8zbZbn8csQqaxqmQ0",
@@ -11,6 +13,15 @@ $(document).ready(function () {
     firebase.initializeApp(config);
     //store data from firebase in database variable
     var database = firebase.database();
+
+    //create current time
+    function currentTime() {
+        var current = moment().format('MMMM Do YYYY, h:mm:ss a');
+        $("#current-time").html(current);
+        setTimeout(currentTime, 1000);
+    };
+
+    currentTime();
 
     //set up click even for submit button
     $("#submit").on("click", function (event) {
@@ -33,7 +44,8 @@ $(document).ready(function () {
 
     //set up the child_added event for firebase train-schedule location
     database.ref("/train-schedule").on("child_added", function (childSnap) {
-        //console.log(childSnap.val());
+        //console.log(childSnap);
+        console.log(childSnap.val());
         //create a new Row using <tr> tag
         var newRow = $("<tr>");
         newRow.addClass("text-center");
@@ -46,33 +58,51 @@ $(document).ready(function () {
         //create a new variblae to store new first arrival
         var newFrequency = $("<td>").text(childSnap.val().frequency);
         console.log(childSnap.val().frequency);
-        
+
         //create a new variblae to store new first arrival
-            // convert first arrival to hh:mm 
+        // convert first arrival to hh:mm 
         var newFirstTrainArrival = moment(childSnap.val().first_arrival, "hh:mm").subtract(1, "years");
-        console.log(newFirstTrainArrival.format()); 
+        console.log(newFirstTrainArrival.format());
         //calculate time difference between now and new first arrival by minutes
         var timeDifferent = moment().diff(newFirstTrainArrival, "minutes")
         console.log(timeDifferent);
-        
+
         //calculate the amount of minute until the next train arrives
         var minuteAway = childSnap.val().frequency - (timeDifferent % childSnap.val().frequency);
         console.log(minuteAway);
         var newMinuteAway = $("<td>").text(minuteAway);
-        
+
         //calculate and set the time for the next arrival time
-        var nextArrival = moment().add(minuteAway, "minutes").format('LT'); 
+        var nextArrival = moment().add(minuteAway, "minutes").format('LT');
         console.log(nextArrival);
         var newNextArrival = $("<td>").text(nextArrival);
 
         //create a remove button
         var removeButton = $("<button>");
-        removeButton.addClass("arrival btn btn-danger text-center").attr("data-key", " + key + ");
-        removeButton.text("X");
+        removeButton.addClass("btn btn-danger text-center fa fa-trash");
+        var key = childSnap.key; //store the key of each child into a variable
+        removeButton.attr("data-key", key);
         var newRemoveButton = $("<td>").append(removeButton)
-        
+
         newRow.append(newTrainName, newDestination, newFrequency, newNextArrival, newMinuteAway, newRemoveButton);
         $("#table-body").append(newRow);
-    })
+    }, function (errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    });
+
+    //remove row from remove button
+    $(document).on("click", ".fa-trash", function () {
+        // remove the row when trash button is clicked on html
+        $(this).closest("tr").remove();
+        //remove the data from firebase
+        var keyref = $(this).attr("data-key");//create variable to store the key of each child
+        console.log(keyref);
+        database.ref("/train-schedule").child(keyref).remove();//remove the child 
+    });
+
+    //create function for page to automatically restart
+    setInterval(function () {
+        window.location.reload();
+    }, 50000);
 
 })
